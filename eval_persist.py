1e69c2ddaa81091616ca273abe01698d3533ddd4
Ding Ning <51272259+ding05@users.noreply.github.com>
2022-04-18 17:47:24 +1200

    Add files via upload

--- eval_persist.py
+++ eval_persist.py
@@ -1,8 +1,6 @@
 import numpy as np
 from numpy import load
 
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
 import matplotlib.pyplot as plt
@@ -14,35 +12,17 @@ lead_time = 1
 data_path = 'data/'
 out_path = 'out/'
 
-# Load the node feature matrix and process it fit for linear regression models.
-
-node_features = load(data_path + 'node_features.npy')
 y = load(data_path + 'y.npy')
 
-x = np.transpose(node_features)
+# Create a persistence model.
 
-y = y[lead_time:]
-x = x[:len(x)-lead_time]
+num_examples = len(y)
+num_train = int(num_examples * train_split)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=train_split, shuffle=False)
-
-# Train a multiple linear regression model.
-
-reg = LinearRegression().fit(x_train, y_train)
-
-print("Complete training.")
-print("----------")
-print()
+y_test = y[num_train+lead_time:]
+pred = y[num_train:-lead_time]
 
 # Test the model.
-
-pred = reg.predict(x_test)
-
-for i in range(len(pred)):
-  print('Observed:', y_test[i], '; predicted:', pred[i])
-
-print("----------")
-print()
 
 test_mse = mean_squared_error(pred, y_test, squared=False)
 test_rmse = mean_squared_error(pred, y_test, squared=True)
@@ -54,14 +34,14 @@ print()
 fig, ax = plt.subplots(figsize=(12, 8))
 plt.xlabel('Month')
 plt.ylabel('SSTA')
-plt.title('LR_SSTAGraphDataset_leadtime_1_numsample_1679_trainsplit_0.8_RMSE_' + str(round(test_rmse, 4)), fontsize=12)
+plt.title('Persist_SSTAGraphDataset_leadtime_1_numsample_1679_trainsplit_0.8_RMSE_' + str(round(test_rmse, 4)), fontsize=12)
 blue_patch = mpatches.Patch(color='blue', label='Predicted')
 red_patch = mpatches.Patch(color='red', label='Observed')
 ax.legend(handles=[blue_patch, red_patch])
 month = np.arange(0, len(y_test), 1, dtype=int)
 ax.plot(month, pred, 'o', color='blue')
 ax.plot(month, y_test, 'o', color='red')
-plt.savefig(out_path + 'plot_LR_SSTAGraphDataset_leadtime_1_numsample_1679_trainsplit_0.8.png')
+plt.savefig(out_path + 'plot_persist_SSTAGraphDataset_leadtime_1_numsample_1679_trainsplit_0.8.png')
 
 print("Save the observed vs. predicted plot.")
 print("--------------------")
