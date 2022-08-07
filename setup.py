3db4f4af3948d2047f36d1202a6f0a433d433f52
Ding Ning <51272259+ding05@users.noreply.github.com>
2022-08-07 23:54:40 +1200

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='GraphWaves',
-    version='0.2.3.3',
+    version='0.2.3.4',
     packages=find_packages(),
     url='https://github.com/ding05/GraphWaves',
     license='MIT',
