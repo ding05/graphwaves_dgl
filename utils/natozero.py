7b286a632d8fed304908e886c454b58f7d787f6b
Ding NING <ning.di@northeastern.edu>
2022-10-18 07:19:17 +1300

    Delete natozero.py

--- utils/natozero.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-def natozero(arr, *args, **kwarg):
-    assert isinstance(arr, np.ndarray)
-    dropped=pd.DataFrame(arr).dropna(*args, **kwarg).values
-    if arr.ndim==1:
-        dropped=dropped.flatten()
-    return dropped
